b21888d376710b8861a0a2c63bda2cc3659b6fb5
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-23 10:36:04 -0700

    user can login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by_user_name(params[:user_name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to '/'
